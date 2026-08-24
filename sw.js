4aa6ec20245379e6542a237a738d27cfec9e008d
gemmafcassells <gemma.f.cassells@gmail.com>
2026-08-24 15:05:46 +0100

    Auto-fill brand and name from Parfumo fetch

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'atlas-of-scent-v8';
+const CACHE = 'atlas-of-scent-v9';
 const SHELL = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (e) => {
