f44c2d198e44e332fe263cdef47591974e55883b
gemmafcassells <gemma.f.cassells@gmail.com>
2026-08-24 11:55:52 +0100

    Add Fragrantica photo fetch + notes paste-parse

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'atlas-of-scent-v4';
+const CACHE = 'atlas-of-scent-v5';
 const SHELL = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (e) => {
