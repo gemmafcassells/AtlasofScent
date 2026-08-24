dbf914da2e4ebfdfbb8eb04e46354a0f9a5c1dff
gemmafcassells <gemma.f.cassells@gmail.com>
2026-08-24 12:47:34 +0100

    Auto-fill family and category from Parfumo accords

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'atlas-of-scent-v6';
+const CACHE = 'atlas-of-scent-v7';
 const SHELL = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (e) => {
